957122b2a1864f50d917fa861cfe7b39e0fee647
Phil Suslov <tayge123@mail.ru>
2026-02-07 16:23:22 +0300

    OrderService.V4

--- inventory/cmd/main.go
+++ inventory/cmd/main.go
@@ -1,4 +1,19 @@
 package main
 
+import (
+	inV1 "github.com/PhilSuslov/homework/shared/pkg/proto/inventory/v1"
+)
+
+type InventoryService struct {
+	inV1.UnimplementedInventoryServiceServer
+	parts map[string]*inV1.Part
+}
+
+func NewInventoryServiceClient() *InventoryService {
+	return &InventoryService{
+		parts: make(map[string]*inV1.Part),
+	}
+}
+
 func main() {
 }
--- order/cmd/main.go
+++ order/cmd/main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"context"
 	"sync"
 	"time"
-	"github.com/PhilSuslov/homework/shared/pkg/openapi/order/v1"
+
+	orderV1 "github.com/PhilSuslov/homework/shared/pkg/openapi/order/v1"
+	inventoryV1 "github.com/PhilSuslov/homework/shared/pkg/proto/inventory/v1"
+	paymentV1 "github.com/PhilSuslov/homework/shared/pkg/proto/payment/v1"
+
+
+	"github.com/google/uuid"
 )
 
 const (
@@ -14,26 +21,76 @@ const (
 
 type OrderStorage struct{
 	mu sync.RWMutex
-	Orders map[string]*orderV1.OrderDto
+	orders map[string]*orderV1.OrderDto
 }
+
+type OrderHandler struct {
+	storage *OrderStorage
+}
+
+type CreateOrderOK struct {
+	OrderUUID  string  `json:"order_uuid"`
+	TotalPrice float64 `json:"total_price"`
+}
+
+func (CreateOrderOK) CreateOrderRes() {}
 
 func NewOrderStorage() *OrderStorage{
 	return &OrderStorage{
-		Orders: make(map[string]*orderV1.OrderDto),
+		orders: make(map[string]*orderV1.OrderDto),
 	}
+}
+
+func NewOrderHandler(storage *OrderStorage) *OrderHandler{
+	return &OrderHandler{
+		storage: storage,
+	}
+}
+
+func (h *OrderHandler) CreateOrder(ctx context.Context, request *orderV1.CreateOrderRequest) (orderV1.CreateOrderRes, error){
+	h.storage.mu.Lock()
+	defer h.storage.mu.Unlock()
+
+	orderUUID := uuid.New()
+
+	order := &orderV1.OrderDto{
+		OrderUUID: orderUUID,
+		UserUUID: request.UserUUID,
+		PartUuids: request.PartUuids,
+		TotalPrice: 1443, //request., InventoryService
+		// TransactionUUID:, Тут пока хз где брать информацию
+    	// PaymentMethod: ,
+    	// Status: ,
+	}
+
+	if _, ok := h.storage.orders[orderUUID.String()]; ok{
+		return &orderV1.BadRequestError{
+			Code: 400, Message: "Bad request",
+		}, nil
+	}
+	h.storage.orders[orderUUID.String()] = order
+	// return &CreateOrderOK{
+	// 	OrderUUID: orderUUID,
+	// 	TotalPrice: order.TotalPrice,
+	// }, nil
 }
 
 func (o *OrderStorage) GetOrderByUUID(uuid string) *orderV1.OrderDto{
 	o.mu.RLock()
 	defer o.mu.RUnlock()
 
-	order,ok := o.Orders[uuid]
+	order,ok := o.orders[uuid]
 	if !ok{
 		return nil
 	}
 	return order
 }
 
+
 func main(){
-	GetOrderByUUID()
+	storage := NewOrderStorage()
+
+	orderHandler := NewOrderHandler(storage)
+
+	orderService, err := orderV1.NewServer(orderHandler)
 }
